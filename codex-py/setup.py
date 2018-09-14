6168d15e65f0390ab24b4bcc6ee07ee5af01c917
Eric Gustafson <eg-git@elfwerks.org>
2018-09-14 00:05:29 -0600

    codex: Dockerfile

---
+++ codex-py/setup.py
@@ -0,0 +1,13 @@
+## setuptools  (https:/pythonhosted.org/setuptools/setuptools.html)
+
+from setuptools import setup, find_packages
+
+setup(
+    name = "codex",
+    version = "0.1",
+    packages = ["codex"],
+    classifiers = [
+        'Programming Language :: Python',
+        'Development Status :: 1 - Planning',
+    ],
+)
